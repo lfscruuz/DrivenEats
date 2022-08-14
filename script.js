a482320dc3a4c81547f68ac7451f95648d84a547
Luís Felipe Cruz <lfscruz2001@gmail.com>
2022-08-14 18:07:10 -0300

    highlight pronto

--- script.js
+++ script.js
@@ -2,24 +2,35 @@
 function selectMeal(mealClass){
 
     const meal = document.querySelector(mealClass);
-    const selectedMeal = document.querySelector('.highlight');
-    const checkmark = selectedMeal.querySelector('.greenCheckmark');
+    const selectedMeal = document.querySelector('.meal .highlight');
+    // const checkmark = selectedMeal.querySelector('.checkmark');
 
     if (selectedMeal !== null){
         selectedMeal.classList.remove('highlight');
-        meal.classList.remove('greenCheckmark')
+        // meal.classList.remove('greenCheckmark')
     }
-
-    console.log(meal)
-    console.log(selectedMeal)
-    console.log(checkmark)
-
-
     meal.classList.add('highlight');
-    checkmark.classList.add('greenCheckmark')
+    // checkmark.classList.add('greenCheckmark')
+}
 
-    
+function selectBeverage(beverageClass){
 
-    
+    const beverage = document.querySelector(beverageClass);
+    const selectedBeverage = document.querySelector('.beverage .highlight');
 
+    if (selectedBeverage !== null){
+        selectedBeverage.classList.remove('highlight');
+    }
+    beverage.classList.add('highlight');
+}
+
+function selectDessert(dessertClass){
+
+    const dessert = document.querySelector(dessertClass);
+    const selectedDessert = document.querySelector('.dessert .highlight');
+
+    if (selectedDessert !== null){
+        selectedDessert.classList.remove('highlight');
+    }
+    dessert.classList.add('highlight');
 }
