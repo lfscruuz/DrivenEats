04f89e47ccd861663fb24114ccd1d7c6d22e5cb8
Luís Felipe Cruz <lfscruz2001@gmail.com>
2022-08-14 11:34:17 -0300

    comeco da implementacao de js

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-let option = document.getElementsByClassName("option");
 
-set
+function selectMeal(mealClass){
 
-function highlight(){
-    alert('selecionado');
+    const meal = document.querySelector(mealClass);
+    const selectedMeal = document.querySelector('.highlight');
+    const checkmark = selectedMeal.querySelector('.greenCheckmark');
+
+    if (selectedMeal !== null){
+        selectedMeal.classList.remove('highlight');
+        meal.classList.remove('greenCheckmark')
+    }
+
+    console.log(meal)
+    console.log(selectedMeal)
+    console.log(checkmark)
+
+
+    meal.classList.add('highlight');
+    checkmark.classList.add('greenCheckmark')
+
+    
+
+    
+
 }
