7ccff63d43d418e7cc77f52f182fc4ce9b7a978e
Luís Felipe Cruz <lfscruz2001@gmail.com>
2022-08-15 15:08:37 -0300

    ultima versao

--- script.js
+++ script.js
@@ -1,36 +1,59 @@
+let selectedMeal = document.querySelector('.meal .highlight');
+let selectedBeverage = document.querySelector('.beverage .highlight');
+let selectedDessert = document.querySelector('.dessert .highlight');
+
 
 function selectMeal(mealClass){
 
     const meal = document.querySelector(mealClass);
-    const selectedMeal = document.querySelector('.meal .highlight');
+    selectedMeal = document.querySelector('.meal .highlight');
     // const checkmark = selectedMeal.querySelector('.checkmark');
 
     if (selectedMeal !== null){
         selectedMeal.classList.remove('highlight');
-        // meal.classList.remove('greenCheckmark')
+        // meal.classList.remove('greenCheckmark');
     }
     meal.classList.add('highlight');
-    // checkmark.classList.add('greenCheckmark')
+    // checkmark.classList.add('greenCheckmark');
+
+    console.log(selectedMeal);
+
 }
 
 function selectBeverage(beverageClass){
 
     const beverage = document.querySelector(beverageClass);
-    const selectedBeverage = document.querySelector('.beverage .highlight');
+    selectedBeverage = document.querySelector('.beverage .highlight');
 
     if (selectedBeverage !== null){
         selectedBeverage.classList.remove('highlight');
     }
     beverage.classList.add('highlight');
+
+    console.log(selectedBeverage);
 }
 
 function selectDessert(dessertClass){
 
     const dessert = document.querySelector(dessertClass);
-    const selectedDessert = document.querySelector('.dessert .highlight');
+    selectedDessert = document.querySelector('.dessert .highlight');
 
     if (selectedDessert !== null){
         selectedDessert.classList.remove('highlight');
     }
     dessert.classList.add('highlight');
+
+    console.log(selectedDessert);
 }
+
+
+function finalOrder(){
+    const greyButton = document.querySelector('.greyButton');
+    const greenButton = document.querySelector('.greenButton');
+
+    if (selectedMeal && selectedBeverage && selectedDessert !== null){
+        greenButton.classList.remove('hidden')
+        greyButton.classList.add('hidden');
+    }
+}
+
